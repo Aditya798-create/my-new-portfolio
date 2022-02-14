d7503bf465bafc3d6e84b400f50f3335df0e8c74
Aditya Vashistha <77147323+Aditya798-create@users.noreply.github.com>
2022-02-14 06:46:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".input", {
+  strings: ["I am Learner", "I am Creator", "I am Student", "I am Youtuber"],
+  typeSpeed: 120,
+  backSpeed: 110,
+  loop: true,
+});
+
+var typed = new Typed(".input-2", {
+  strings: ["Learner", "Creator", "Student", "Youtuber"],
+  typeSpeed: 120,
+  backSpeed: 110,
+  loop: true,
+});
